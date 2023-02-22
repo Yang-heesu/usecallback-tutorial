d71fa121b27b1f584b2e7e6967101541dc7b4d23
Yang-heesu <hsge0602@gmail.com>
2023-02-23 00:43:40 +0900

    someFunction에 useCallback 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 function App() {
   const [number, setNumber] = useState(0);
@@ -7,10 +7,16 @@ function App() {
   //someFunction은 함수 객체가 들어있는 메모리의 주소값을 가지고 있다.
   //컴포넌트가 렌더링되면 함수 객체가 새로 만들어진다.
   //그래서 number값이 바뀌어도 useEffect가 호출된다.
-  const someFunction = () => {
+  // const someFunction = () => {
+  //   console.log(`someFunc : number : ${number}`);
+  //   return;
+  // };
+
+  //number가 바뀔 때마다 메모리제이션된 함수가 업데이트 된다. 
+  const someFunction = useCallback(() => {
     console.log(`someFunc : number : ${number}`);
     return;
-  };
+  }, [number]);
 
   useEffect(() => {
     console.log("someFucntion이 변경되었습니다.");
