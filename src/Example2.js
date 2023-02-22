bfea9ab15ca81f2be274f346e1ee600b87feba0d
Yang-heesu <hsge0602@gmail.com>
2023-02-23 01:09:12 +0900

    useCallback 적용

--- src/Example2.js
+++ src/Example2.js
@@ -1,25 +1,41 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import Box from "./Box";
 
 function Example2() {
   const [size, setSizes] = useState(100);
+  const [isDark, setIsDark] = useState(false);
 
-  const createBoxStyle = () => {
+  // const createBoxStyle = () => {
+  //   return {
+  //     backgroundColor:'pink',
+  //     width: `${size}px`,
+  //     height: `${size}px`
+  //   }
+  // }
+
+  //size가 바뀔 때만 초기화!
+  //Change Theme을 클릭해도 Box의 useEffect가 호출되지 않는다.
+  const createBoxStyle = useCallback(() => {
     return {
       backgroundColor:'pink',
       width: `${size}px`,
       height: `${size}px`
     }
-  }
+  }, [size]);
 
   return (
-    <div>
+    <div
+      style={{
+        background: isDark ? 'black' : 'white'
+      }}
+    >
       <input
         type="number"
         value={size}
         onChange={(e) => setSizes(e.target.value)}
       />
+      <button onClick={() => setIsDark(!isDark)}>Change Theme</button>
       <Box createBoxStyle={createBoxStyle}/>
     </div>
   );
