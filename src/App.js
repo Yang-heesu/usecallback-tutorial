4287cba148b669192cc1c103fe7262b835990bb5
Yang-heesu <hsge0602@gmail.com>
2023-02-23 00:46:03 +0900

    toggle 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useCallback } from "react";
 
 function App() {
   const [number, setNumber] = useState(0);
+  const [toggle, setToggle] = useState(true);
 
   //함수형 컴포넌트는 state를 변경할 때마다 렌더링된다.
   //someFunction은 함수 객체가 들어있는 메모리의 주소값을 가지고 있다.
@@ -13,6 +14,7 @@ function App() {
   // };
 
   //number가 바뀔 때마다 메모리제이션된 함수가 업데이트 된다. 
+  //toggle버튼을 눌러도 호출되지 않음.
   const someFunction = useCallback(() => {
     console.log(`someFunc : number : ${number}`);
     return;
@@ -29,6 +31,7 @@ function App() {
         value={number}
         onChange={(e) => setNumber(e.target.value)}
       />
+      <button onClick={(e) => setToggle(!toggle)}>{toggle.toString()}</button>
       <br/>
       <button onClick={someFunction}>Call someFunc</button>
     </div>
